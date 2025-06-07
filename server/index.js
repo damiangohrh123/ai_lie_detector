de36cb9bca4a7d8441ad7a52bf21463750eeffc0
damian <damiangohrh@gmail.com>
2025-06-07 15:08:41 +0800

    Reorganize folder structure.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import facialRoutes from './routes/facial.js';
 
 const app = express();
 
+app.use(cors()); // 👈 This enables CORS
 app.use(express.json({ limit: '50mb' }));
 app.use('/api', facialRoutes);
 
